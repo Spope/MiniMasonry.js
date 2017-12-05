1b4cf1cfba45c78f2d74380c60c7cc5bf45e81ad
Thomas Pinaud <pinaudt@gmail.com>
2017-12-05 16:57:53 +0100

    Fixed file name

--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
             },
             home: {
                 files: {
-                    './build/minimasonry.js' : [
+                    './build/minimasonry.min.js' : [
                         './src/minimasonry.js'
                     ]
                 }
